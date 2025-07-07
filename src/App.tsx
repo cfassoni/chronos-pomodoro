79990f3111312c483e845028861f12b40fdb6a1e
Celso Fassoni <cfassoni@users.noreply.github.com>
2025-07-07 16:20:11 -0300

    section 2, lecture 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
-
 import './styles/theme.css';
 import './styles/global.css';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-        provident iusto error dolore pariatur voluptatum fuga consectetur harum
-        quo nulla, nostrum accusantium tempora alias voluptate. Odio sequi aut
-        deserunt rem.
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
