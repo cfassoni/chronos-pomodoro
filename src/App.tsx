47f18c5b45d39efcf2e7db225c422e0331a2b762
Celso Fassoni <cfassoni@users.noreply.github.com>
2025-07-08 13:32:48 -0300

    section 2, lecture 32

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,21 @@ import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
+import { Heading } from './components/Heading';
 
 export function App() {
+  let numero = 0;
+
+  function handleClick() {
+    numero += 1;
+    console.log(numero, Date.now());
+  }
+
   return (
     <>
+      <Heading>Número: {numero}</Heading>
+      <button onClick={handleClick}>Aumenta</button>
+
       <Container>
         <Logo />
       </Container>
@@ -30,7 +41,7 @@ export function App() {
         <form className='form' action=''>
           <div className='formRow'>
             <DefaultInput
-              labelText='task'
+              labelText={numero.toString()}
               id='meuInput'
               type='text'
               placeholder='Digite algo'
